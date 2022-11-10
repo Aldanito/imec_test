f2cb3d6c48598ec5732b2233d46cd743dade370f
Aldan M <111426307+Aldanito@users.noreply.github.com>
2022-11-10 08:49:59 +0600

    Aldan changes
    Siu

--- app.py
+++ app.py
@@ -1,45 +1,25 @@
-from calendar import c
 from datetime import datetime
 from urllib import request
 from flask import Flask, render_template,session, abort, redirect, request, url_for, send_from_directory,g,flash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.event import listens_for
-from flask_admin import Admin, expose, AdminIndexView,form
+from flask_admin import Admin,form, BaseView,expose
 from flask_admin.contrib.sqla import ModelView
 from flask_admin.base import MenuLink
-from os.path import exists
 import os.path as op 
 from flask_migrate import Migrate
 from flask_ckeditor import CKEditor, CKEditorField, upload_fail, upload_success
-from markupsafe import Markup
-from flask_babel import Babel, gettext as _  # $ pip install flask-babel
 import os
-import werkzeug
-from flask_mail import Mail
-
+from flask_mail import Mail, Message
+import psycopg2
 
 migrate = Migrate()
 basedir = os.path.abspath(os.path.dirname(__file__))
 file_path = op.join(op.dirname(__file__), 'static/files')
-
-# available languages
-LANGUAGES = {
-    'en': {'flag':'en', 'name':'English'},
-    'ru': {'flag':'ru', 'name':'Русский'},
-    'kk': {'flag':'kk', 'name':'Қазақша'},
-}
-
-
-d = {
-    'Read More': 'Read More', 
-    'Return': 'Return', 
-    'Previous': 'Previous', 
-    'Next':'Next'
-    }
+cert_path = op.join(op.dirname(__file__), 'static/cert')
 
 
 app = Flask(__name__)
-babel = Babel(app)
 
 app.config['FLASK_APP'] = 'app.py'
 app.config['FLASK_ENV'] = 'development'
@@ -49,8 +29,16 @@ app.config['CKEDITOR_FILE_UPLOADER'] = 'upload'
 
 
 
-
-mail = Mail(app)
+   
+# configuration of mail
+app.config['MAIL_SERVER']='smtp.office365.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = '1155121354@link.cuhk.edu.hk'
+app.config['MAIL_PASSWORD'] = 'Nfp%^hc5pjpm3d$'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['FLASK_ADMIN_SWATCH'] = 'flatly'   #lumen cyborg flatly
+mail = Mail(app) # instantiate the mail class
 
 ckeditor = CKEditor(app)
 
@@ -62,40 +50,63 @@ admin = Admin(app, template_mode='bootstrap4')
 
 
 
+@app.route("/forward/<string:user>/<string:phone>", methods=['GET','POST'])
+def move_forward(user,phone):
+    conn = psycopg2.connect(
+        host="127.0.0.1",
+        database="corp",
+        user='postgres',
+        password='ALDANM')
+
+    cursor = conn.cursor()
+    sql = '''SELECT email from username'''
+    # sql = '''DELETE FROM post1'''
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    emails = []
+    for row in rows:
+        emails.append("{0}".format(row[0]))
+    print(emails)
+    msg = Message(
+                    'Заказ Звонка',
+                    sender ='1155121354@link.cuhk.edu.hk',
+                    recipients = emails
+                )
+    msg.body = 'Поступил заказ от:\nКлиент:' +user +'\nНомер Телефона:' +phone +'\n'
+
+    mail.send(msg)
+
+    return redirect('/')
 
 @app.route('/')
 def index():
     session['logged_in'] = False
     session.clear()
-    return redirect(url_for('lang', lang=g.lang))
+    return render_template('index1.html')
+
+# @babel.localeselector
+# def get_locale():
+#     return g.lang
 
 
-@babel.localeselector
-def get_locale():
-    return g.lang
+# @app.before_request
+# def before_request():
+#     if request.view_args and 'lang' in request.view_args:
+#         g.lang = request.view_args['lang']
+#         request.view_args.pop('lang')
+#     elif request.args.get('lang'):
+#         g.lang = request.args.get('lang')
+#     else:
+#         g.lang = request.accept_languages.best_match({'en':'en', 'kz':'kz', 'ru':'ru'}.keys()) or 'en'
+#     if g.lang not in {'en':'en', 'kz':'kz', 'ru':'ru'}:
+#         abort(404)
 
 
-@app.before_request
-def before_request():
-    if request.view_args and 'lang' in request.view_args:
-        g.lang = request.view_args['lang']
-        request.view_args.pop('lang')
-    elif request.args.get('lang'):
-        g.lang = request.args.get('lang')
-    else:
-        g.lang = request.accept_languages.best_match(LANGUAGES.keys()) or 'en'
-    if g.lang not in LANGUAGES:
-        abort(404)
-
-
-@app.route('/<lang>')
-def lang():
-    return render_template('index1.html', l=_('en'), who=_('MAIN PAGE'), header_1=_("NEWS"), header_2=_("ITEMS"), header_3=_("SERVICES"), header_4=_("PURCHASING"), header_5=_("ABOUT"), header_6=_("VACANCIES"), header_7=_("CONTACTS"))
-
-count = 0
+# @app.route('/')
+# def lang():
+#     return render_template('index1.html', lang = g.lang)
     
 class ImageView(ModelView):
-    count+=1
     def is_accessible(self):
         if "logged_in" in session:  
             return True
@@ -109,7 +120,40 @@ class ImageView(ModelView):
                                       base_path=file_path)
     }
 
-    
+
+class CertView(ModelView):
+    def is_accessible(self):
+        if "logged_in" in session:  
+            return True
+        else:
+            abort(403)
+    form_overrides = dict(content=CKEditorField)
+    create_template = 'cert.html'
+    edit_template = 'cert.html'
+    form_extra_fields = {
+        'path': form.ImageUploadField('Image',
+                                      base_path=cert_path)
+    }
+
+class UserView(ModelView):
+    def is_accessible(self):
+        if "logged_in" in session:  
+            return True
+        else:
+            abort(403)
+    create_template = 'user_view.html'
+    edit_template = 'user_view.html'
+
+
+class User(db.Model):
+    __tablename__ = 'username'
+    id = db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String)
+    email = db.Column(db.String)
+    login = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(64))
+
+
 class Post(db.Model):
     __tablename__ = "post1"
     id = db.Column(db.Integer, primary_key=True)
@@ -123,51 +167,71 @@ class Post(db.Model):
     path = db.Column(db.Unicode(128), nullable=False)
 
 
-@app.route('/<lang>/login', methods = ["GET", "POST"])
+class Certificates(db.Model):
+    __tablename__ = "cert"
+    id = db.Column(db.Integer, primary_key=True)
+    title_en = db.Column(db.String(), unique=False, nullable=False)
+    title_ru = db.Column(db.String(), unique=False, nullable=False)
+    title_kz = db.Column(db.String(), unique=False, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    path = db.Column(db.Unicode(128), nullable=False)
+
+# @app.route('/<lang>/login', methods = ["GET", "POST"])
+# def login():
+#     if request.method == "POST":
+#         if request.form.get("username") == "admin" and  request.form.get("password") == "123456":
+#             session['logged_in'] = True
+#             return redirect("/admin")
+#         else:
+#             return render_template("login.html",login_1=_("Admin IMEC Login"),login_2=_("Sign in"), failed = True)
+
+#     return render_template('login.html',login_1=_("Admin IMEC Login"),login_2=_("Sign in"))
+
+@app.route('/login', methods = ["GET", "POST"])
 def login():
     if request.method == "POST":
         if request.form.get("username") == "admin" and  request.form.get("password") == "123456":
             session['logged_in'] = True
             return redirect("/admin")
         else:
-            return render_template("login.html",login_1=_("Admin IMEC Login"),login_2=_("Sign in"), failed = True)
+            return render_template("login.html", failed = True)
 
-    return render_template('login.html',login_1=_("Admin IMEC Login"),login_2=_("Sign in"))
-
+    return render_template('login.html')
 
 
 @app.route('/logout')
 def logout():
     session['logged_in'] = False
     session.clear()
-    return redirect('/ru/login')
+    return redirect('/login')
 
 
-@app.route('/<lang>/news/<string:lol>')
-def newsstr(lol, lang):
-    g.lang = lang
-    return redirect(lang+'/news/1')
+@app.route('/news/<string:lol>')
+def newsstr():
+    return redirect('/news/1')
     
 
-@app.route('/<lang>/news')
-def news(lang):
-    g.lang = lang
-    return redirect(lang+'/news/1')
+@app.route('/news')
+def news():
+    return redirect('/news/1')
 
 
-@app.route('/<lang>/news/<int:page_num>')
+@app.route('/news/<int:page_num>')
 def news_articles(page_num):
    
     news_articles = Post.query.order_by(Post.id.desc()).paginate(page=page_num, per_page=4, error_out = True)
-    return render_template("news.html", news_articles=news_articles, next = _(d['Next']), previous = _(d['Previous']), read = _(d['Read More']), l=_('en'),lang = lang,  who=_('MAIN PAGE'), header_1=_("NEWS"), header_2=_("ITEMS"), header_3=_("SERVICES"), header_4=_("PURCHASING"), header_5=_("ABOUT"), header_6=_("VACANCIES"), header_7=_("CONTACTS"))
+    return render_template("news.html", news_articles=news_articles)
 
+@app.route('/cert')
+def cert():
+    cert = Certificates.query.order_by(Certificates.id.desc())
+    return render_template("cert_page.html", cert=cert)
 
-@app.route('/<lang>/news_page/<int:num>')
+@app.route('/news_page/<int:num>')
 def news_page(num):
     if num:
-        print(Post.id)
         news_articles = Post.query.filter_by(id= num)
-        return render_template('news_page.html', back = _(d['Return']), news_articles=news_articles,  l=_('en'),lang = lang,  who=_('MAIN PAGE'), header_1=_("NEWS"), header_2=_("ITEMS"), header_3=_("SERVICES"), header_4=_("PURCHASING"), header_5=_("ABOUT"), header_6=_("VACANCIES"), header_7=_("CONTACTS"))
+        return render_template('news_page.html',news_articles=news_articles)
     else:
         return abort(403)
 
@@ -186,27 +250,24 @@ def upload(target):
     extension = f.filename.split('.')[-1].lower()
     if extension not in ['jpg', 'gif', 'png', 'jpeg']:
         return upload_fail(message='Image only!')
-    # if exists(file_path+/)
-    # print(app.config['UPLOADED_PATH']+'/'+f.filename)
     flash(f(app.config['UPLOADED_PATH']+'/'+f.filename), 'success')
     f.save(os.path.join(app.config['UPLOADED_PATH'], target.id))
     url = url_for('uploaded_files', filename=target.id)
     return upload_success(url=url)
-@app.route('/admin/post/edit', methods=['POST'])
-def gettik(target):
-    f = request.files.get('upload')
-    extension = f.filename.split('.')[-1].lower()
-    if extension not in ['jpg', 'gif', 'png', 'jpeg']:
-        return upload_fail(message='Image only!')
-    # if exists(file_path+/)
-    print(app.config['UPLOADED_PATH']+'/'+f.filename)
-    # flash(f(app.config['UPLOADED_PATH']+'/'+f.filename), 'success')
-    # f.save(os.path.join(app.config['UPLOADED_PATH'], target.id))
-    # url = url_for('uploaded_files', filename=target.id)
-    # return upload_success(url=url)
+
+
+# @app.route('/admin/post/edit', methods=['POST'])
+# def gettik(target):
+#     f = request.files.get('upload')
+#     extension = f.filename.split('.')[-1].lower()
+#     if extension not in ['jpg', 'gif', 'png', 'jpeg']:
+#         return upload_fail(message='Image only!')
+#     print(app.config['UPLOADED_PATH']+'/'+f.filename)
+
+
 
 @listens_for(Post, 'after_delete')
-def del_image(mapper, connection, target):
+def del_image(target):
     if target.path:
         try:
             print(target.id)
@@ -214,18 +275,19 @@ def del_image(mapper, connection, target):
         except OSError:
             pass
 
-
-def rename_img():
-    for count, filename in enumerate(os.listdir(file_path)):
-        dst = f"NEWS_IMG {str(count)}.jpg"
-        src =f"{file_path}/{filename}"  # foldername/filename, if .py file is outside folder
-        dst =f"{file_path}/{dst}"
-        os.rename(src, dst)
-    flash(f"Account Succesfully created", "success")
-    print(file_path)    
+@listens_for(Certificates, 'after_delete')
+def del_image(target):
+    if target.path:
+        try:
+            print(target.id)
+            os.remove(op.join(cert, target.path))
+        except OSError:
+            pass
 
 
 admin.add_view(ImageView(Post, db.session, name='News'))
+admin.add_view(CertView(Certificates, db.session, name='Certificates'))
+admin.add_view(UserView(User, db.session, name='User'))
 admin.add_link(MenuLink(name='Main page', endpoint='index'))
 admin.add_link(MenuLink(name='Logout', endpoint='logout'))
 
--- static/js/main1.js
+++ static/js/main1.js
@@ -102,3 +102,48 @@
     
 })(jQuery);
 
+let langs = ['en', 'ru', 'kz'];
+let lang = 'ru';
+
+setLangStyles(lang);
+
+
+
+var name = localStorage.getItem('name');
+function setStyles(styles) {
+  var elementId = '__lang_styles';
+  var element = document.getElementById(elementId);
+  if (element) {
+    element.remove();
+  }
+
+  let style = document.createElement('style');
+  style.id = elementId;
+  style.type = 'text/css';
+
+  if (style.styleSheet) {
+    style.styleSheet.cssText = styles;
+  } else {
+    style.appendChild(document.createTextNode(styles));
+  }
+  document.getElementsByTagName('head')[0].appendChild(style);
+}
+
+function setLang(lang) {
+  setLangStyles(lang);
+}
+
+function setLangStyles(lang) {
+  let styles = langs
+    .filter(function (l) {
+      return l != lang;
+    })
+    .map(function (l) {
+      return ':lang('+ l +') { display: none; }';
+    })
+    .join(' ');
+
+  setStyles(styles);
+
+  
+}
